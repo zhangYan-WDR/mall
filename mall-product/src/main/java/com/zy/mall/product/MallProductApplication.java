ff764aa91e3531f086b5e07db603ef52fdece501
zhangyan <yan.zhang@zhishengai.com>
2023-10-11 23:11:04 +0800

    添加product服务的mybatis-plus的配置，并且将springboot版本进行适配

---
+++ mall-product/src/main/java/com/zy/mall/product/MallProductApplication.java
@@ -0,0 +1,29 @@
+package com.zy.mall.product;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 商品服务
+ * 1、整合MyBatis-Plus
+ *     1）、导入依赖
+ *     2）、配置
+ *       1）、配置数据源
+ *          1）、导入数据库驱动
+ *          2）、在application.yml配置数据源相关信息
+ *       2）、配置MyBatis-Plus
+ *          1）、使用@MapperScan
+ *          2）、告诉MyBatis-Plus，sql映射文件位置
+ *          mybatis-plus:
+ *          mapper-locations: classpath:mapper/*.xml
+ */
+@SpringBootApplication
+@MapperScan("com.zy.mall.product.dao")
+public class MallProductApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MallProductApplication.class, args);
+    }
+
+}
