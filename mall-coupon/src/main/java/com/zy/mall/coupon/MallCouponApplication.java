5106a9eb058941ba89045b6ea9cd21d5ce8d74f5
zhangyan <yan.zhang@zhishengai.com>
2023-10-25 23:34:56 +0800

    nacos作为配置中心将不同服务的不同配置进行分类放在对应命名空间中

--- mall-coupon/src/main/java/com/zy/mall/coupon/MallCouponApplication.java
+++ mall-coupon/src/main/java/com/zy/mall/coupon/MallCouponApplication.java
@@ -6,6 +6,7 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
 @EnableDiscoveryClient // 开启服务注册发现功能
+//每个微服务创建自己的命名空间，使用配置分组区分相应环境
 public class MallCouponApplication {
 
     public static void main(String[] args) {
