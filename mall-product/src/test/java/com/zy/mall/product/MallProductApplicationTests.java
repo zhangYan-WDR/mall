eeeaff2d4d2c337c46fbdc67ced2d43f3d4dc8c0
zhangyan <yan.zhang@zhishengai.com>
2023-11-11 09:47:00 +0800

    创建集成第三方服务oss，并使用服务端签名直传的方式

--- mall-product/src/test/java/com/zy/mall/product/MallProductApplicationTests.java
+++ mall-product/src/test/java/com/zy/mall/product/MallProductApplicationTests.java
@@ -1,15 +1,14 @@
 package com.zy.mall.product;
 
-import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
-import com.zy.mall.product.entity.BrandEntity;
+
 import com.zy.mall.product.service.BrandService;
-import org.junit.jupiter.api.Test;
+
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
-import java.util.List;
+
 
 @SpringBootTest
 @RunWith(SpringRunner.class)
@@ -17,21 +16,31 @@ class MallProductApplicationTests {
 
 	@Autowired
 	BrandService brandService;
-	@Test
-	void contextLoads() {
+//	@Autowired
+//	OSSClient ossClient;
+//	@Test
+//	void contextLoads() {
+//
+////		BrandEntity brandEntity = new BrandEntity();
+////		brandEntity.setBrandId(1L);
+////		brandEntity.setName("华为");
+////		brandService.updateById(brandEntity);
+//////		brandService.save(brandEntity);
+////		System.out.println("保存成功");
+//
+//		LambdaQueryWrapper<BrandEntity> brandEntityLambdaQueryWrapper = new LambdaQueryWrapper<>();
+//		brandEntityLambdaQueryWrapper.eq(BrandEntity::getBrandId, 1L);
+//		List<BrandEntity> list = brandService.list(brandEntityLambdaQueryWrapper);
+//		System.out.println(list);
+//
+//	}
 
-//		BrandEntity brandEntity = new BrandEntity();
-//		brandEntity.setBrandId(1L);
-//		brandEntity.setName("华为");
-//		brandService.updateById(brandEntity);
-////		brandService.save(brandEntity);
-//		System.out.println("保存成功");
 
-		LambdaQueryWrapper<BrandEntity> brandEntityLambdaQueryWrapper = new LambdaQueryWrapper<>();
-		brandEntityLambdaQueryWrapper.eq(BrandEntity::getBrandId, 1L);
-		List<BrandEntity> list = brandService.list(brandEntityLambdaQueryWrapper);
-		System.out.println(list);
 
-	}
+//	@Test
+//	public void ossUpload() throws FileNotFoundException {
+//		InputStream inputStream = new FileInputStream("/Users/zhangyan/Desktop/1.jpg");
+//		ossClient.putObject("zy-mall-shopping","1.jpg",inputStream);
+//	}
 
 }
